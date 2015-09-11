86f336dd0470b8178164059b5559399f05709a6c
Dagur Arinbjörn Daníelsson <dagurdan@simnet.is>
2015-09-11 16:31:16 +0000

    SOUNDS (this was way harder than expected)

--- DotsProj/app/src/main/java/com/ru/dots/dotsproj/BoardView.java
+++ DotsProj/app/src/main/java/com/ru/dots/dotsproj/BoardView.java
@@ -11,6 +11,11 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Point;
 import android.graphics.RectF;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
+import android.media.MediaPlayer;
+import android.media.ToneGenerator;
 import android.preference.PreferenceManager;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
@@ -33,6 +38,8 @@ public class BoardView extends View {
     private List<Integer> m_colors = new ArrayList<Integer>(); // Halda utan um litina
     private List<ArrayList<Integer>> m_points = new ArrayList<ArrayList<Integer>>(); // two dim array to hold point colors
     ValueAnimator animator = new ValueAnimator();
+    private ToneGenerator m_toneGen = new ToneGenerator(AudioManager.STREAM_MUSIC, 50);
+    private List<Integer> m_tones = new ArrayList<Integer>();
 
     private int NUM_CELL;
     private RectF m_circle = new RectF();
@@ -44,7 +51,6 @@ public class BoardView extends View {
 
     public BoardView(Context context, AttributeSet attrs) {
         super(context, attrs);
-
         setColorArray();
         m_paint.setColor(Color.BLACK);
         m_paint.setStyle(Paint.Style.FILL_AND_STROKE);
@@ -76,6 +82,74 @@ public class BoardView extends View {
         }
     }
 
+
+    // this funtion come from http://stackoverflow.com/a/13565100 user Xarp
+    public void playTone(double freq){
+        double duration = 0.2f;                // seconds
+        double freqOfTone = freq;           // hz
+        int sampleRate = 8000;              // a number
+        double dnumSamples = duration * sampleRate;
+        dnumSamples = Math.ceil(dnumSamples);
+        int numSamples = (int) dnumSamples;
+        double sample[] = new double[numSamples];
+        byte generatedSnd[] = new byte[2 * numSamples];
+        for (int i = 0; i < numSamples; ++i) {      // Fill the sample array
+            sample[i] = Math.sin(freqOfTone * 2 * Math.PI * i / (sampleRate));
+        }
+        // convert to 16 bit pcm sound array
+        // assumes the sample buffer is normalized.
+        // convert to 16 bit pcm sound array
+        // assumes the sample buffer is normalised.
+        int idx = 0;
+        int i = 0 ;
+        int ramp = numSamples / 20 ;                                    // Amplitude ramp as a percent of sample count
+        for (i = 0; i< ramp; ++i) {                                     // Ramp amplitude up (to avoid clicks)
+            double dVal = sample[i];
+            // Ramp up to maximum
+            final short val = (short) ((dVal * 32767 * i/ramp));
+            // in 16 bit wav PCM, first byte is the low order byte
+            generatedSnd[idx++] = (byte) (val & 0x00ff);
+            generatedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
+        }
+        for (i = i; i< numSamples - ramp; ++i) {                        // Max amplitude for most of the samples
+            double dVal = sample[i];
+            // scale to maximum amplitude
+            final short val = (short) ((dVal * 32767));
+            // in 16 bit wav PCM, first byte is the low order byte
+            generatedSnd[idx++] = (byte) (val & 0x00ff);
+            generatedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
+        }
+        for (i = i; i< numSamples; ++i) {                               // Ramp amplitude down
+            double dVal = sample[i];
+            // Ramp down to zero
+            final short val = (short) ((dVal * 32767 * (numSamples-i)/ramp ));
+            // in 16 bit wav PCM, first byte is the low order byte
+            generatedSnd[idx++] = (byte) (val & 0x00ff);
+            generatedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
+        }
+        AudioTrack audioTrack = null;                                   // Get audio track
+        try {
+            audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
+                    sampleRate, AudioFormat.CHANNEL_CONFIGURATION_MONO,
+                    AudioFormat.ENCODING_PCM_16BIT, (int)numSamples*2,
+                    AudioTrack.MODE_STATIC);
+            audioTrack.write(generatedSnd, 0, generatedSnd.length);     // Load the track
+            audioTrack.play();                                          // Play the track
+        }
+        catch (Exception e){
+
+        }
+
+        int x =0;
+        do{                                                     // Montior playback to find when done
+            if (audioTrack != null)
+                x = audioTrack.getPlaybackHeadPosition();
+            else
+                x = numSamples;
+        }while (x<numSamples);
+
+        if (audioTrack != null) audioTrack.release();
+    }
 
     @Override
     protected void onMeasure( int widthMeasureSpec, int heightMeasureSpec ) {
@@ -171,8 +245,8 @@ public class BoardView extends View {
         if (event.getAction() == MotionEvent.ACTION_DOWN){
             m_moving = true;
             m_cellPath.add(new Point(xToCol(x), yToRow(y)));
+            playTone(100);
         }else if (event.getAction() == MotionEvent.ACTION_MOVE){
-            // TODO: sound on new item in path (higher pitch with longer path)
             if (m_moving){
                 if (!m_cellPath.isEmpty()){
                     int col = xToCol(x);
@@ -185,6 +259,8 @@ public class BoardView extends View {
                             && (Math.abs(col - last.x) == 1 || Math.abs(row - last.y) == 1)
                             && dx <= 1 && dy <= 1 && dx != dy) {
                         m_cellPath.add(new Point(col, row));
+                        playTone(100 * m_cellPath.size());
+                        //m_toneGen.startTone(m_tones.get(m_cellPath.size()), 200);
                     }
                 }
                 m_circle.offsetTo(x, y);
